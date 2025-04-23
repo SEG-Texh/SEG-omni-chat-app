c8da261252bd57af4407ab0d203a91f4a97283e7
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro.local>
2025-04-23 19:39:54 +0000

    Updated webhook handling and added new routes

--- controllers/facebookController.js
+++ controllers/facebookController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
-const VERIFY_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const receivedMessages = [];
 
 // Webhook Verification
