c9e2fb0a55dad6237fb435441ee85173d9bdc97d
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-05-27 16:35:20 +0000

    update app

---
+++ controllers/adminController.js
@@ -0,0 +1,15 @@
+exports.getAllUsers = (req, res) => {
+  res.json({ message: 'All users' });
+};
+
+exports.getAllMessages = (req, res) => {
+  res.json({ message: 'All messages' });
+};
+
+exports.getStats = (req, res) => {
+  res.json({ message: 'Stats' });
+};
+
+exports.createUser = (req, res) => {
+  res.json({ message: 'User created' });
+};
