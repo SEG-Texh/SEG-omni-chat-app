192f7a811bcf76e91b85d9fe9cb1a4bb8dad697c
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-05-29 18:02:37 +0000

    update app

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const Message = require('../models/Message');
+const Message = require('../models/message');
 const ActivityLog = require('../models/ActivityLog');
 
 module.exports = {
