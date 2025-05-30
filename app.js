2be98981c3fe1ed03f086c49b69473ad5f5acaa8
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-05-30 19:56:46 +0000

    update app

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 
-
+app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
