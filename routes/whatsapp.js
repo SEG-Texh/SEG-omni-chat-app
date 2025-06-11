23d761cef10763c9bed4e394f480df5455b610f6
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-06-11 08:57:17 +0000

    update app

--- routes/whatsapp.js
+++
@@ -1,9 +0,0 @@
-// routes/whatsapp.js
-const express = require('express');
-const router = express.Router();
-const whatsappController = require('../controllers/whatsappController');
-
-router.get('/webhook', whatsappController.verifyWhatsAppWebhook);
-router.post('/webhook', whatsappController.handleWhatsAppWebhook);
-
-module.exports = router;
