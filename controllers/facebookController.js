a308dfc3da779e61523755818c7bbf0c9fd940f4
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-06-14 21:16:14 +0000

    update app

--- controllers/facebookController.js
+++ controllers/facebookController.js
@@ -1,4 +1,4 @@
-const Message = require('../models/Message');
+const Message = require('../models/message');
 const { getIO } = require('../config/socket');
 const User = require('../models/User');
 
