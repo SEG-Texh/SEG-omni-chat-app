edf7caf0adb512ffa336519e75f7d3fb2edca933
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-06-14 18:56:52 +0000

    update app

--- routes/facebook.js
+++ routes/facebook.js
@@ -1,9 +1,14 @@
 // routes/facebook.js
 const express = require('express');
 const router = express.Router();
-const facebookController = require('./controllers/facebookController');
-const { getIO } = require('./config/socket');
-const io = getIO();
+const facebookController = require('../controllers/facebookController');
+const { getIO } = require('../config/socket');
+router.post('/webhook', (req, res) => {
+  const io = require('../config/socket').getIO();
+  io.emit('message', req.body); // Example
+  res.sendStatus(200);
+});
+
 
 
 router.get('/webhook', facebookController.verifyFacebookWebhook);
