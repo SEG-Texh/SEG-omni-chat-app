6cd3100732a3b829186c0bb1baa076796c9752be
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-06-16 09:30:20 +0000

    update app

--- controllers/facebookController.js
+++ controllers/facebookController.js
@@ -13,6 +13,24 @@ const facebookController = (() => {
       return 'Facebook User';
     }
   };
+  // Option 1: If defined in the same file (add this somewhere above where it's used)
+const verifyFacebookWebhook = (req, res) => {
+  const VERIFY_TOKEN = process.env.FACEBOOK_WEBHOOK_VERIFY_TOKEN;
+
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+};
+
 
   const processMessageEvent = async (event, pageId, io) => {
     try {
