52946aae2b33c23d0741b482651ea206cf463745
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-06-25 08:00:48 +0000

    update app

--- models/userStats.js
+++ models/userStats.js
@@ -1,4 +1,7 @@
-// In models/UserStats.js
+// models/UserStats.js
+
+const mongoose = require('mongoose');
+
 const userStatsSchema = new mongoose.Schema({
   totalUsers: { type: Number, default: 0 },
   lastUpdated: { type: Date, default: Date.now }
