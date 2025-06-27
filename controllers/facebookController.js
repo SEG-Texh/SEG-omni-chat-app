3ab8e66aaf0fe2cd415d4b3f87ca05179a562cd3
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-06-27 14:11:50 +0000

    update app

--- controllers/facebookController.js
+++ controllers/facebookController.js
@@ -86,7 +86,7 @@ class FacebookController {
         platformMessageId: message.mid,
         platformSenderId: senderPsid,
         platformRecipientId: pageId,
-        status: 'receivded'
+        status: 'received'
       };
 
       if (message.attachments) {
