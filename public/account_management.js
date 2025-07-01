42b721aaba274d00499a3650eaf050d614c8a766
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-07-01 10:29:07 +0000

    update app

--- public/account_management.js
+++ public/account_management.js
@@ -1,5 +1,4 @@
 // Account Management page specific functionality
-let showMessage = null // Declare showMessage variable
 
 document.addEventListener("DOMContentLoaded", () => {
   loadAccountsData()
