707b4b9e44111537ddc8908cd691d718898ad7df
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-07-02 15:32:25 +0000

    Mount all routes in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// Enable CORS for all routes
 app.use(cors());
 app.use(express.json());
 
@@ -9,10 +10,18 @@ app.use(express.json());
 const authRoutes = require('./routes/auth');
 const facebookRoutes = require('./routes/facebook');
 const whatsappRoutes = require('./routes/whatsapp');
+const userRoutes = require('./routes/users');
+const conversationRoutes = require('./routes/conversation');
+const messageRoutes = require('./routes/messages');
+const dashboardRoutes = require('./routes/dashboard');
 
 // Mount routes
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/facebook', facebookRoutes);
+app.use('/api/conversations', conversationRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 app.use('/whatsapp', whatsappRoutes);
 
 module.exports = app;
