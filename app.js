d5d490080a691204d90f680429e0e70cc914fe96
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-07-02 15:33:11 +0000

    Simplify app.js to middleware configuration

--- app.js
+++ app.js
@@ -6,22 +6,4 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-const authRoutes = require('./routes/auth');
-const facebookRoutes = require('./routes/facebook');
-const whatsappRoutes = require('./routes/whatsapp');
-const userRoutes = require('./routes/users');
-const conversationRoutes = require('./routes/conversation');
-const messageRoutes = require('./routes/messages');
-const dashboardRoutes = require('./routes/dashboard');
-
-// Mount routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/facebook', facebookRoutes);
-app.use('/api/conversations', conversationRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/dashboard', dashboardRoutes);
-app.use('/whatsapp', whatsappRoutes);
-
 module.exports = app;
