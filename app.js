f8d6b21f05495470180c8c01bbf3dea17bd8a132
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-07-02 15:23:16 +0000

    Add auth routes to app.js

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
+const authRoutes = require('./routes/auth');
 const facebookRoutes = require('./routes/facebook');
 const whatsappRoutes = require('./routes/whatsapp');
 
-
+// Mount routes
+app.use('/api/auth', authRoutes);
 app.use('/api/facebook', facebookRoutes);
 app.use('/whatsapp', whatsappRoutes);
-
 
 module.exports = app;
