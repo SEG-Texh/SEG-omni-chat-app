6f7fe4427176dd0650bceb6306e840c5b94c6dd0
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-07-05 08:18:15 +0000

    update app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: [
     "./public/**/*.{html,js}",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./public/styles.css",
+    "./public/styles/*.css",
   ],
   theme: {
     extend: {},
