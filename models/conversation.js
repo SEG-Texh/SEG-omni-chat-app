7364f781f848771193affbb023cfb0513626d600
DANIEL ABOAGYE <monty@DANIELs-MacBook-Pro-3.local>
2025-07-05 14:00:47 +0000

    update app

--- models/conversation.js
+++ models/conversation.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const ConversationSchema = new Schema({
   platform: { type: String, default: 'facebook' },
   platformConversationId: { type: String, unique: true }, // Unique identifier for the conversation on the platform
-  participants: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Facebook user IDs or your user IDs
+  participants: [{ type: String }], // Facebook user IDs or your user IDs
   lastMessage: { type: Schema.Types.ObjectId, ref: 'Message' },
   unreadCount: { type: Number, default: 0 }
 }, { timestamps: true });
